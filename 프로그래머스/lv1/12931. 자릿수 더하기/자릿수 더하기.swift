a30bb0905082dcafbceb6d666cc22aefcb4e2989
Doy Kim <kimdee.dev@gmail.com>
2022-12-26 17:32:29 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.00 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ n:Int) -> Int
+{
+    var answer:Int = 0
+    var number:Int = n
+    while number > 0 {
+        answer += number % 10
+        number = number / 10
+    }  
+    return answer
+}
