8b89b52d2f4d4ba1d6aaffed1926970162f4c4b0
Doy Kim <heydoyhey@gmail.com>
2024-03-03 18:40:30 +0900

    Update 사과나무.swift

--- 백준/Gold/19539. 사과나무/사과나무.swift
+++ 백준/Gold/19539. 사과나무/사과나무.swift
@@ -1,19 +1,19 @@
 /*
 참고: https://readble-ko.tistory.com/149
-
 */
 
-let n = Int(readLine()!)! // 사과나무갯수
+let apple = Int(readLine()!)! // 사과나무갯수
 var array = readLine()!.split(separator:" ").map { Int(String($0))!}
-var count = 0
-var sum = 0
+var cnt = 0 ; var sum = 0
 
-for i in 0..<n {
+for i in 0..<apple {
     sum += array[i]
-    count += array[i]/2
+    cnt += array[i]/2 //2의 배수가 되어도 OK, 어차피 물뿌리개는 동시에 사용해야되므로 
 }
 
-if (sum % 3 != 0 ) { print("NO") }
-else if (count >= sum/3) { print("YES") } 
-else { print("NO")}
+if (sum % 3 != 0 ) { print("NO") } // 3의 배수가 되면 OK
+else {
+    let answer = cnt >= sum/3 ? "YES": "NO"
+    print(answer)
+}
 
